bd4b289e820f4adfa88439bbcee9eb21b63f6d38
Stian Steinbakken <stian.steinbakken@posten.no>
2024-10-03 15:06:49 +0200

    chore: formatting

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,41 +7,37 @@
  */
 
 plugins {
-    // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
-    alias(libs.plugins.kotlin.jvm)
-
-    // Apply the application plugin to add support for building a CLI application in Java.
-    application
+  // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
+  alias(libs.plugins.kotlin.jvm)
+  // Apply the application plugin to add support for building a CLI application in Java.
+  application
 }
 
 repositories {
-    // Use Maven Central for resolving dependencies.
-    mavenCentral()
+  // Use Maven Central for resolving dependencies.
+  mavenCentral()
 }
 
 dependencies {
-    // This dependency is used by the application.
-    implementation(libs.guava)
+  // This dependency is used by the application.
+  implementation(libs.guava)
 }
 
 testing {
-    suites {
-        // Configure the built-in test suite
-        val test by getting(JvmTestSuite::class) {
-            // Use Kotlin Test test framework
-            useKotlinTest("2.0.0")
-        }
-    }
+  suites {
+    // Configure the built-in test suite
+    val test by
+      getting(JvmTestSuite::class) {
+        // Use Kotlin Test test framework
+        useKotlinTest("2.0.0")
+      }
+  }
 }
 
 // Apply a specific Java toolchain to ease working on different environments.
-java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(21)
-    }
-}
+java { toolchain { languageVersion = JavaLanguageVersion.of(21) } }
 
 application {
-    // Define the main class for the application.
-    mainClass = "org.example.AppKt"
+  // Define the main class for the application.
+  mainClass = "org.example.AppKt"
 }
